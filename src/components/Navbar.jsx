c2db77321f50ca7e402022dd1d736bd42f37dcf9
23Ousama <Elyazidi2001@gmail.com>
2025-05-27 17:24:54 +0000

    Terminado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
+    const { store, dispatch } = useGlobalReducer();
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+    const handleRemoveFavorite = (uid, type) => {
+        dispatch({ type: "REMOVE_FAVORITE", payload: { uid, type } });
+    };
+
+    return (
+        <nav className="navbar navbar-dark bg-dark mb-3">
+            <div className="container-fluid">
+                <Link to="/">
+                    <span className="navbar-brand mb-0 h1">STAR WARS</span>
+                </Link>
+                <div className="ml-auto">
+                    <Link to="/favoritos" className="btn btn-purple position-relative">
+                        Favoritos
+                        <span className="badge bg-secondary ms-2">{store.favoritos.length}</span>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'  // Global styles for your application
-import { RouterProvider } from "react-router-dom";  // Import RouterProvider to use the router
-import { router } from "./routes";  // Import the router configuration
-import { StoreProvider } from './hooks/useGlobalReducer';  // Import the StoreProvider for global state management
+import './index.css'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { StoreProvider } from './hooks/useGlobalReducer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Main = () => {
     return (
-        <React.StrictMode>  
-            {/* Provide global state to all components */}
-            <StoreProvider> 
-                {/* Set up routing for the application */} 
+        <React.StrictMode>
+            <StoreProvider>
                 <RouterProvider router={router}>
                 </RouterProvider>
             </StoreProvider>
@@ -18,5 +20,4 @@ const Main = () => {
     );
 }
 
-// Render the Main component into the root DOM element.
 ReactDOM.createRoot(document.getElementById('root')).render(<Main />)
